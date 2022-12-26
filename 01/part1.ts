c3970aa55510cf26e37b22b84122f608cb3b6ace
0x15d3f <rayhan@0x15.dev>
2022-12-26 02:54:10 +0000

    removed usage of Math API

--- 01/part1.ts
+++ 01/part1.ts
@@ -4,14 +4,17 @@ const calorieList = (() =>
   fs.readFileSync(`${__dirname}/calories.txt`, { encoding: "utf8" }))();
 
 const largestElfCals = (calorieList: string): number => {
-  const individualElfsCalories = calorieList.split("\n\n").map((calorieBlock) =>
-    calorieBlock
-      .split("\n")
-      .map((arbitraryCalorie) => parseInt(arbitraryCalorie))
-      .reduce((calA, calB) => calA + calB)
-  )
+  const individualElfsCalories = calorieList
+    .split("\n\n")
+    .map((calorieBlock) =>
+      calorieBlock
+        .split("\n")
+        .map((arbitraryCalorie) => parseInt(arbitraryCalorie))
+        .reduce((calA, calB) => calA + calB)
+    )
+    .sort((a, b) => a - b)[0];
 
-  return Math.max(...individualElfsCalories);
+  return individualElfsCalories;
 };
 
 console.log(largestElfCals(calorieList));
