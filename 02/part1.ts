3d201f9ca81250232ad341d3ecc791527ef6b208
0x15d3f <rayhan@0x15.dev>
2022-12-26 16:33:43 +0000

    day 02 solutions

---
+++ 02/part1.ts
@@ -0,0 +1,27 @@
+import fs from "fs";
+
+const rawEncryptedStrategies = (() =>
+  fs.readFileSync(`${__dirname}/strategies.txt`, { encoding: "utf8" }))();
+
+// Map possible outcomes -> points won;
+const possiblePairOutcomes: { [key: string]: number } = {
+  "A X": 4,
+  "B X": 1,
+  "C X": 7,
+  "A Y": 8,
+  "B Y": 5,
+  "C Y": 2,
+  "A Z": 3,
+  "B Z": 9,
+  "C Z": 6,
+};
+
+const computePairsOutcome = (strategies: string): number => {
+  const choicePairs = strategies
+    .split("\n")
+    .map((pair) => possiblePairOutcomes[pair])
+    .reduce((a, b) => a + b);
+  return choicePairs;
+};
+
+console.log(computePairsOutcome(rawEncryptedStrategies));
---
+++ 02/part2.ts
@@ -0,0 +1,24 @@
+import fs from "fs";
+
+const rawEncryptedStrategies = (() =>
+  fs.readFileSync(`${__dirname}/strategies.txt`, { encoding: "utf8" }))();
+
+// Map possible outcomes -> needed action -> points;
+const possiblePairOutcomes: {
+  [key: string]: { [key: string]: number };
+} = {
+  A: { X: 3, Y: 4, Z: 8 },
+  B: { X: 1, Y: 5, Z: 9 },
+  C: { X: 2, Y: 6, Z: 7 },
+};
+
+const computePairsOutcome = (strategies: string): number => {
+  const totalScoreOutcome = strategies
+    .split("\n")
+    .map((pair) => pair.split(" "))
+    .map((round) => possiblePairOutcomes[round[0]][round[1]])
+    .reduce((roundA, roundB) => roundA + roundB, 0);
+  return totalScoreOutcome;
+};
+
+console.log(computePairsOutcome(rawEncryptedStrategies));
